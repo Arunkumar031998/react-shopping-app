a2e968973f281f91e64d9de3dae40acdf452ff1a
Arun <arunkumarofcl@gmail.com>
2021-11-08 12:24:33 +0530

    product component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import Products from "./components/Products";
+import data from "./data.json"
 
-function App() {
-  return (
-    <div className="App">
-      arun
-    </div>
-  );
+
+ class App extends React.Component {
+   constructor(){
+     super();
+     this.state = {
+       products: data.products,
+       size: "",
+       sort: "",
+     }
+   }
+   render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}/></div>
+            <div className="sidebar">sidebar</div>
+          </div>
+        </main>
+        <footer>All copyright</footer>
+      </div>
+    );
+   }
+ 
 }
 
 export default App;
